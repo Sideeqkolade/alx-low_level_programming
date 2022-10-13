17899de6cc47700633cbd12ef8e34af62d70344e
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-13 16:20:01 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,6 @@
 /*
  * main - Entry point
  *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
  * Return: Always 0 (success)
  */
 int main(void)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if no error
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outxome is a piece of art,\n");
+	return (0);
+}
