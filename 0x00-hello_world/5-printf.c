432cec2e6a2bcd42b4d2caba1b954fcfed6711ed
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-13 16:58:02 -0700

    task 5 redone

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: 0 if no errors
  */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
