722c83bc0c420dc87bf663373258c2b25a9cfb72
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-14 17:57:46 -0700

    task 3 redone

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,13 @@ int main(void)
 {
 	int ch;
 
-	for (ch = "a"; ch <= "z"; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-	for (ch = "A"; ch <= "Z"; ch++)
-	{
-		putchar(ch);
-	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
