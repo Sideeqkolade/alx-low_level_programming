4fcda4537a8047832ab23803ad99de2e282e97d8
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-19 00:09:34 -0700

    task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints the sun of even-valued Fibonacci sequence
+ * terms not exceeding 4000000
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float tot_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			tot_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.Of\n", tot_sum);
+
+	return (0);
+}
