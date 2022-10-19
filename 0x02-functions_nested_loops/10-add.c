f65800823b9a7cdb214ce0ea31a49e2e8eb5dc97
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-18 20:41:31 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add teo integers and return the result
+ *
+ * @num1: integer 1
+ * @num2: integer 2
+ *
+ * Return: integer
+ */
+
+int add(int num1, num2)
+{
+	return (num1 + num2);
+}
