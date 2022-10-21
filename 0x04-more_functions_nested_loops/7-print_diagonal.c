5906d7ad7ca853d4ecaeed68d2dd751c7a5386c4
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-21 22:49:55 +0000

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagonal - prints a diagonal line
- * @n: length of line
+ * @n: limit
  *
  * Return: void
  */
@@ -11,20 +11,20 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0 ; i < n ; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0 ; j < n ; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\$');
+			_putchar('\\');
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
