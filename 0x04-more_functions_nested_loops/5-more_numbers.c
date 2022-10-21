eb2c8713e44369f309af7c0adc280caba38014dd
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-21 16:37:22 +0000

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i <= 10; i++)
 	{
+		int j;
+
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
 				_putchar('0' + j / 10);
 			}
-			_putchar('0' + i % 10);
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
