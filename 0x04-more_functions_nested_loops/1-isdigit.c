12a25fdf715adfc45145341a9a8d67dcd8e772ef
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-20 18:55:27 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isdigit - chevk for a digit (0 through 9)
+ * _isdigit - check for a digit (0 through 9)
  * @c: integer
  * Return: 1 if it is a digit and zero if it's not
  */
 
 int _isdigit(int c)
 {
-	if (c >='0' && c <= '9')
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
