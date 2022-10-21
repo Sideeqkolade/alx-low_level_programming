e6c47553e8fe069724e940daf850d60f1b9d661b
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-20 21:29:47 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,18 +3,19 @@
 /**
  * print_numbers - print numbers from zero to nine
  *
- * Return: void
+ * Return: Always 0 (success)
  */
 
 void print_numbers(void)
 {
-	int i;
 
-	i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
+int i;
 
-		_putchar ('\n');
-	}
+i = 0;
+
+for (i = 0; i < 10; i++)
+
+_putchar(i + '0');
+
+_putchar ('\n');
 }
