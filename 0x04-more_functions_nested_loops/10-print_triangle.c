6f0e8aa0ec887b2b0e6893da97c363cd8d05203c
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-22 00:15:59 +0000

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,6 +12,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
@@ -19,20 +20,17 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; i++)
+			for (j = size - i; j > 0; j--)
 			{
-				if ((size - i - 1) > j)
-				{
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar('#');
-				}
-				_putchar('\n');
+				_putchar(' ');
 			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 }
