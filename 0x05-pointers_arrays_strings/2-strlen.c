809b26d960db2050f6d5392f78a47f8676acbb24
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-24 22:38:30 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: character s
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; c[a] != '\0'; c++)
+	{
+	}
+
+	return (c);
+}
