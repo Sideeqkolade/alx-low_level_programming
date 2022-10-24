8a4c69a6ed19072487005baac6e9bd98c6dc5b68
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-24 20:38:31 +0000

    task 0 redone

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and has a value of 98
- * @n - the parameter of the function
+ * @n: the parameter of the function
  * Return: void
  */
 
