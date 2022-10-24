60d59545e742af19e00343396f0cb9677ca07519
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-24 23:09:38 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	a--;
+	for (b = a; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
