647b81edb3eecf538ff537617944a3e1ff80d94c
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-31 15:39:25 -0700

    task 0,1,2

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a readme file for tasks on more pointers, araays and strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void set_string(char **s, char *to);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+#endif
