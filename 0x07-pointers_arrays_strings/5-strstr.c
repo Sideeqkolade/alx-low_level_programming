378178435e2ff852caac5fa031bdfdabd94c145a
Sideeqkolade <sideeqkolade@gmail.com>
2022-10-31 17:51:53 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,10 +12,6 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	if (needle[0] == '\0')
-	{
-		return (haystack);
-	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
@@ -23,8 +19,8 @@ char *_strstr(char *haystack, char *needle)
 			if (haystack[i + j] != needle[j])
 				break;
 		}
-		if (needle[j] == '\0')
-			return (haystack + i);
+		if (!needle[j])
+			return (haystack[i]);
 	}
-	return ('\0');
+	return (NULL);
 }
