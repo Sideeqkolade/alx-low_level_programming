48d9231859f15d8a3f400cb10d67e44a263c98d0
Sideeqkolade <sideeqkolade@gmail.com>
2022-11-17 05:36:51 +0000

    tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - the basic info o the dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
