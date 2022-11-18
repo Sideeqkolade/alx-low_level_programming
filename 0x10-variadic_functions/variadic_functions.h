8746c89e65ee6b496445f8c5d108fa11bea70c6d
Sideeqkolade <sideeqkolade@gmail.com>
2022-11-18 04:46:10 +0000

    task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,4 +19,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_c(va_list c);
+void print_s(va_list s);
+void print_i(va_list i);
+void print_f(va_list f);
 #endif
