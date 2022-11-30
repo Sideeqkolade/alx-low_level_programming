f1c8d064080a50f5ffe9eed02358fc00fe7c61be
Sideeqkolade <sideeqkolade@gmail.com>
2022-11-30 00:16:48 +0000

    fix 4 task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,9 @@
 listint *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *last_node;
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node;
+       
+	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 	{
@@ -20,18 +22,19 @@ listint *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	last_node = *head;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
 	}
 	else
 	{
-		last_node = *head;
-
 		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
 		}
+
 		last_node->next = new_node;
 	}
 
