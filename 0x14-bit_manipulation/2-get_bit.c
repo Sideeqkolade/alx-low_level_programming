ea2be4a75764941e9bfad9000634dc4c09646c20
Sideeqkolade <sideeqkolade@gmail.com>
2022-12-29 07:50:58 +0000

    updated task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,18 +10,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	unsigned int hold;
 
-	if (n == 0 && index < 64)
-		return (0);
+	if (index > 64)
+		return (-1);
 
-	for (i = 0; i <= 63; n >> 1; i++)
-	{
-		if (index == i)
-		{
-			return (n & 1);
-		}
-	}
+	hold = n >> index;
 
-	return (-1);
+	return (hold & 1);
 }
